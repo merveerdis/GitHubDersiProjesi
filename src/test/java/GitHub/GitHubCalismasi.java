68fb8838c1347e4c923f133555994b179d2b2d32
merveerdis <102983625+merveerdis@users.noreply.github.com>
2022-04-07 12:16:47 +0200

    Update GitHubCalismasi.java

--- src/test/java/GitHub/GitHubCalismasi.java
+++ src/test/java/GitHub/GitHubCalismasi.java
@@ -5,6 +5,9 @@ public class GitHubCalismasi {
     public static void main(String[] args) {
 
         System.out.println("GitHub Dersi Basladi!");
+        
+        System.out.println("GitHub Dersinde Basarilar!");
+
 
     }
 
